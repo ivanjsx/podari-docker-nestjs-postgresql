a4424bfe98fed866c000840b48f6d8d48d2b3178
Ivan Shanin <ivanxshanin@gmail.com>
2024-09-09 01:18:31 +0300

    fix backend URL for production environment

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:4000";
+export const URL = "https://api.kpd.study.ivanjsx.com";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
